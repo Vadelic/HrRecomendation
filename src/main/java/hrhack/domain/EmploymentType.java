3084f7892dd97b45aa2c345adb496a24639f2ee1
Vadim <komyshenets@mail.ru>
2019-02-28 16:32:24 +0300

    vacancy type

---
+++ src/main/java/hrhack/domain/EmploymentType.java
@@ -0,0 +1,9 @@
+package hrhack.domain;
+
+/**
+ * Created by Komyshenets on 28.02.2019.
+ */
+public enum EmploymentType {
+    FULL,
+    NOT_FULL
+}
--- src/main/java/hrhack/domain/Vacancy.java
+++ src/main/java/hrhack/domain/Vacancy.java
@@ -1,16 +1,19 @@
 package hrhack.domain;
 
+import java.util.List;
+
 /**
  * Created by Komyshenets on 28.02.2019.
  */
 public class Vacancy {
     private String title;
-    private String employmentType;
+    private EmploymentType employmentType;
     private String workHours;
     private String description;
     private Salary salary;
     private Requirements requirements;
     private Address adress;
     private String additionalInfo;
+    private List<String> competition;
 
 }
